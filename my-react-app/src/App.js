775413f603eaa018b72143d9e8ac6f75ddef02aa
Annique Nsabimana <anniquensabimana@gmail.com>
2019-08-06 18:03:10 -0500

    completed: Run initial Install, Run axios, run semantic-ui, created components

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import Card from './components/Card'
+import 'semantic-ui-css/semantic.min.css'; 
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      data: [],
+      followers: []
+    };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  }
+  componentDidMount() {
+    axios.get(`https://api.github.com/users/AnniqueN`)
+      .then(res => this.setState(res))
+  }
+  render() {
+
+    return (<Card data={this.state.data} />)
+
+  }
 }
 
 export default App;
